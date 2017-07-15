fbc552bf70b13bc9f2dad2e3cfcd5b46fe821bb3
izyrider <izyrider27@gmail.com>
2017-07-15 17:00:11 +0900

    회원가입 테이블 생성
    회원가입 테이블 생성 쿼리

--- myBoard/sql/create_db_table.sql
+++ myBoard/sql/create_db_table.sql
@@ -7,4 +7,24 @@ CREATE TABLE board
    name		  VARCHAR(64) NOT NULL,
    path       VARCHAR(256),
    contents   VARCHAR(4096) NOT NULL
-)
+);
+
+CREATE TABLE register
+(
+	userID VARCHAR(20) PRIMARY KEY,
+	userPassword VARCHAR(20),
+	userName VARCHAR(20),
+	userAge INT,
+	userGender VARCHAR(20),
+	userEmail VARCHAR(20)
+);
+
+CREATE TABLE register
+(
+	userID VARCHAR(20) PRIMARY KEY,
+	userPassword VARCHAR(20),
+	userName VARCHAR(20),
+	userAge INT,
+	userGender VARCHAR(20),
+	userEmail VARCHAR(20)
+);
